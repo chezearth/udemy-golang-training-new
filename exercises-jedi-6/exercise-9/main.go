da848edd3aed7244a18c6269e5f760d019bd0aa6
Charles Rethman <rethman@me.com>
2019-03-10 18:27:18 +0200

    Added in exercise 6-9

--- exercises-jedi-6/exercise-9/main.go
+++ exercises-jedi-6/exercise-9/main.go
@@ -3,17 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	x := 40
-	y := 2
-	fmt.Printf("%T\n", x+y)
+	t := bar(sum, []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}...)
+	fmt.Println(t)
 
-	foo(func() int {
-		return 42
-	})
-
+	linkUp(concat, []string{"fred", " ", "john", " ", "luca"}...)
 }
 
-func foo(bar func() int) int {
-	fmt.Println(bar())
-	return bar()
+func bar(f func(x ...int) int, y ...int) int {
+	var s []int
+	for _, v := range y {
+		if v%2 == 0 {
+			s = append(s, v)
+		}
+	}
+	total := f(s...)
+
+	return total
+}
+
+func sum(x ...int) int {
+	n := 0
+	for _, v := range x {
+		n += v
+	}
+	return n
+}
+
+func concat(x ...string) string {
+	var n string
+	for _, v := range x {
+		n = n + v
+	}
+	return n
+}
+
+func linkUp(f func(ss ...string) string, s ...string) {
+	var si []string
+	for _, v := range s {
+		si = append(si, v)
+	}
+	result := f(si...)
+	fmt.Println(result)
 }
